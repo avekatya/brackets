d385b32dac93c3b83b704a893eb9996ca8bd50c4
AveKatya <katyaverchenko@gmail.com>
2021-03-16 01:59:18 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const brackets = new Map(bracketsConfig);
+	const stack = [];
+
+  	str.split('').forEach(el => brackets.get(stack.slice(-1)[0]) !== el ? stack.push(el) : stack.pop());
+
+  	return stack.length == 0;
 }
+
